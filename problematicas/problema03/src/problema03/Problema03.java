d9604fc975c601221c67f6f5b2a9bfffd06fdd7e
Antonio Ojeda <ajojeda6@utpl.edu.ec>
2024-11-24 11:36:35 -0500

    cambios

--- problematicas/problema03/src/problema03/Problema03.java
+++ problematicas/problema03/src/problema03/Problema03.java
@@ -15,14 +15,11 @@ public class Problema03 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        int contador = 1;
-        int numero = 2;
+        int numero = 1;
         int diferencia = 1;
         while(numero < 37){
-            diferencia += 2;
             numero = numero + diferencia;
-            contador = contador + 1;
-
+            diferencia += 2;
             System.out.println(numero);
     }
     }
