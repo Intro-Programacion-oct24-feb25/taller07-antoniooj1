cd1274d27f4471ce2c424b0f6a5013f3fe4e0d88
Antonio Ojeda <ajojeda6@utpl.edu.ec>
2024-11-23 23:09:40 -0500

    cambios severos

---
+++ problematicas/problema02/src/problema02/Problema02.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package problema02;
+
+/**
+ *
+ * @author admin
+ */
+public class Problema02 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    int contador = 0;
+    int numero = 0;
+    while(numero < 110){
+            contador = contador + 2;
+            numero = numero + contador;
+
+            System.out.println(numero);
+    }
+    }
+}
+
+    
+    
