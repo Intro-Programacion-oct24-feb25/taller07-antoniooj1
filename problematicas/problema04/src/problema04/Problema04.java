be9f66e34357fae690ac3257ed202a4ae8d466ef
Antonio Ojeda <ajojeda6@utpl.edu.ec>
2024-11-25 23:44:55 -0500

    cambios antes que se vaya la luz

--- problematicas/problema04/src/problema04/Problema04.java
+++ problematicas/problema04/src/problema04/Problema04.java
@@ -16,29 +16,28 @@ public class Problema04 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
-       double num = 1;
-       double resta = 0;
-       double suma = 0;
-
-
-
-
-       while(num < 15){
-           num += 2;
-           resta = 1 - 1/num;
-           suma += resta;
-           System.out.printf("1 -"+" 1/"+num+ " %.2f\n",
-                   suma);
-       }
-    }
-}
-
-
-       
-   
+      double numero1= 1;      
+      double numero2 =3;      
+      double cont =1;  
+      double resultado= numero1 + numero2;  
+      
+        System.out.print("1");
+         while(numero2 <= 15){
+             
+            if (cont % 2 != 0) {
                 
-          
+                System.out.print(" - 1/" + numero2); 
+                
+                
+            } else {
+                
+                System.out.print(" + 1/" + numero2);  
+            }
+ 
+            cont= cont +1;
+            numero2 +=2;
+            
+       }
+   }
     
-    
-
+}
